95f1508e3f89e4fccb8ccc603d8b7839283aa9ca
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-05-05 21:40:55 +0300

    first settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,7 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
       }}
-    >
-      React homework template
-    </div>
+    ></div>
   );
 };
